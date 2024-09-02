2623bb7f3d732cbd6754ed2b9335cc629f59093c
Sorokovsky <Sorokovskys@ukr.net>
2024-09-02 22:52:41 +0300

    wocommerce

--- plugins/bulk-edit-prices/plugin.php
+++ plugins/bulk-edit-prices/plugin.php
@@ -1,20 +1,14 @@
 <?php
 /*
-Plugin Name: Bulk edit prices
-Description: This plugin for editing prices of various products
-Author: Andrey Sorokovsky <Sorokovskys@ukr.net>
-Version: 0.0.1
+ * Plugin Name:       Bulk edit Plugin
+ * Description:       Plugin for bulk editing variations of product.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Andrey Sorokovsky <Sorokovskys@ukr.net>
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       bull-editing
+ * Domain Path:       /languages
+ * Requires Plugins:  woocommerce
 */
-
-if(!defined("ABSPATH")) {
-    die;
-}
-class AndreBulkEdit 
-{
-    function __construct() {
-    }
-}
-
-if (class_exists("AndreBulkEdit")) {
-    new AndreBulkEdit();
-}
