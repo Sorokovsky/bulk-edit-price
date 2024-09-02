750c592cdfbfe3a6f218020e8c597218bcf6e38f
Sorokovsky <Sorokovskys@ukr.net>
2024-09-02 23:57:26 +0300

    add settings page

--- plugins/bulk-edit-prices/plugin.php
+++ plugins/bulk-edit-prices/plugin.php
@@ -8,7 +8,27 @@
  * Author:            Andrey Sorokovsky <Sorokovskys@ukr.net>
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       bull-editing
+ * Text Domain:       bulk-editing
  * Domain Path:       /languages
  * Requires Plugins:  woocommerce
 */
+
+if(!defined("ABSPATH")) {
+    exit;
+}
+
+add_action('admin_menu', 'add_bulk_edit_settings');
+function add_bulk_edit_settings() {
+    add_menu_page(
+        esc_html__("Bulk Edit Price", "bulk-editing"),
+        esc_html__("Edit Prices", "bulk-editing"),
+        "manage_options",
+        "bulk-edit-price-settings",
+        "render_bulk_page",
+        'dashicons-edit-page'
+    );
+}
+
+function render_bulk_page() {
+    echo esc_html__("Bulk Edit Price", "bulk-editing");
+}
